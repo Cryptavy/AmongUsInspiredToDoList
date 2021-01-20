f5cc14f5fb55a0cd156754b40f53c6b82704375c
Cryptavy <cryptavy@gmail.com>
2021-01-21 01:47:20 +0800

    Added a class for Heading

--- AmoungUsInspiredTodoList.php
+++ AmoungUsInspiredTodoList.php
@@ -3,3 +3,8 @@
     <title> Objectives for Today </title>
     <link rel="stylesheet" type="text/css" href="styleSheet.css">
 </head>
+<body>
+    <div class="heading">
+        <h2> Todo List application using PHP and MySQL</h2>
+    </div>
+</body>
