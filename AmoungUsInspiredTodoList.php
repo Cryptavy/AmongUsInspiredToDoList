f93b97cded629a1a4a69911971607f91ae4fa2da
Cryptavy <cryptavy@gmail.com>
2021-01-21 01:54:09 +0800

    Added the Table for the Task Holder

--- AmoungUsInspiredTodoList.php
+++ AmoungUsInspiredTodoList.php
@@ -13,5 +13,23 @@
         <input type="text" name="task" class="task_input">
         <button type="submit" class="task_btn" name="submit">Add Task</button>
     </form>
-    
+    <table>
+        <thead>
+        <tr>
+        <th>N</th>
+        <th>Task</th>
+        <th>Action</th>
+        </tr>
+      </thead>
+       	<tbody>
+          <tr>
+             <td>1</td>
+             <td class="task">Task holder</td>
+             <td class="delete">
+             	<a href="#">x</a>
+             </td>
+          </tr>
+        </tbody>
+    </table>
 </body>
+</html>
