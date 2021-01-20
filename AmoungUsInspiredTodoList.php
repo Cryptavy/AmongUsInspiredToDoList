05077791d62f1a5b7a3c272f0f09de55eefe23a9
Cryptavy <cryptavy@gmail.com>
2021-01-21 01:52:18 +0800

    Added the Form

--- AmoungUsInspiredTodoList.php
+++ AmoungUsInspiredTodoList.php
@@ -4,7 +4,14 @@
     <link rel="stylesheet" type="text/css" href="styleSheet.css">
 </head>
 <body>
+    //Header
     <div class="heading">
         <h2> Todo List application using PHP and MySQL</h2>
     </div>
+    //Form
+    <form method="POST" action="ToDoList.php">
+        <input type="text" name="task" class="task_input">
+        <button type="submit" class="task_btn" name="submit">Add Task</button>
+    </form>
+    
 </body>
